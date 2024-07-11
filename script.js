9f47f8ea0e7ea8007acbef9dfec35f395ae6abea
FurtiveApk2 <furtive.web@gmail.com>
2024-07-10 21:18:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -392,19 +392,21 @@ function setCookieDni(name, value, days) {
         const date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
-	}
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+    document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/";
 }
+
 
 // Función para obtener el valor de una cookie
 function getCookieDni(name) {
     const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-	}
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let c = cookies[i].trim();
+        if (c.indexOf(nameEQ) === 0) {
+            return decodeURIComponent(c.substring(nameEQ.length));
+        }
+    }
     return null;
 }
 
